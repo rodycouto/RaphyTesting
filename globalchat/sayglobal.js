a0a1bf0e05a48b41cd7f88f7cb5583dcaba5ea5c
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-05-01 14:57:57 -0300

    Create sayglobal.js

---
+++ globalchat/sayglobal.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+const db = require('quick.db')
+
+exports.run = async (client, message, args) => {
+
+    let Moderador = db.get(`moderadoreschatglobal_${message.author.id}`)
+    if (!Moderador) {
+        message.delete().catch(err => { return })
+        return message.channel.send('⚠️ Este é um comando restrito para Moderadores do Chat Global.')
+    }
+
+    let Mensagem = args.join(" ")
+
+    client.guilds.cache.forEach(guild => {
+
+        let CanaisValidos = guild.channels.cache.find(ch => ch.name === "naya-global-chat")
+
+        if (!CanaisValidos) return
+
+        return CanaisValidos.send(`📢 ${Mensagem}\n \n*~ Naya Global System*`)
+    })
+}
